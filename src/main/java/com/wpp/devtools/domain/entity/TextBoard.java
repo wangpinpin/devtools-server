822cc9bfe2c896f54f50651db32ffe0aea02a025
wangpinpin <442042162@qq.com>
2020-08-18 15:58:27 +0800

    commit

---
+++ src/main/java/com/wpp/devtools/domain/entity/TextBoard.java
@@ -0,0 +1,35 @@
+package com.wpp.devtools.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
+@Table(name = "text_board")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+@GenericGenerator(name = "jpa-uuid", strategy = "uuid")
+public class TextBoard {
+
+  @Id
+  @GeneratedValue(generator = "jpa-uuid")
+  private String id;
+  private String ip;
+  private String content;
+  private java.sql.Timestamp createTime;
+  private java.sql.Timestamp updateTime;
+
+
+}
