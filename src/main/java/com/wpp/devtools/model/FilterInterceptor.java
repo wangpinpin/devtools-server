c5199f53ac13811cbe209d707ceafea7b19789bd
wangpinpin <442042162@qq.com>
2020-08-20 10:59:56 +0800

    commit

--- src/main/java/com/wpp/devtools/model/FilterInterceptor.java
+++ src/main/java/com/wpp/devtools/model/FilterInterceptor.java
@@ -59,6 +59,9 @@ public class FilterInterceptor extends HandlerInterceptorAdapter {
             String uri = request.getRequestURI();
             String key = uri + ip;
 
+            //统计每个IP访问方法次数
+            redistUtil.incr(RedisKeyConfig.COUNT + key, 3600 * 24);
+
             //从redis中获取用户访问的次数(redis中保存的key保存(seconds)秒，redisUtils使用的单位是秒，意思是5秒内重复请求接口限制次数)
             String countString = redistUtil.getString(key);
             if (countString == null) {
